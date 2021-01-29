443b9ec34217252775be8078f108b35eed47674d
wonpyoJang <jangwonpyo0@gmail.com>
2021-01-29 23:48:31 +0900

    [fixed] implement logout button

--- client/web/src/pages/Dashboard/Components/Dashboard.tsx
+++ client/web/src/pages/Dashboard/Components/Dashboard.tsx
@@ -1,41 +1,49 @@
-
-
-import React, { Component } from 'react'
+import React, { useCallback } from 'react';
 import 'antd/dist/antd.css';
 import '../../../index.css';
 import './Dashboard.less';
 import ChangePassword from '../../ChangePassword/ChangePassword';
-import { Layout, Button, Row, Col} from 'antd';
+import { Layout, Button, Row, Col } from 'antd';
 import CSS from 'csstype';
 import { InstanceListContainer } from '../Container/InstanceListContainer';
+import { useHistory } from 'react-router-dom';
 
-const { Header, Footer, } = Layout;
-const mypageStyle: CSS.Properties ={
-    margin: '0 auto',
-    float: 'right',
-    marginRight: '5px'
-}
+const { Header, Footer } = Layout;
+const mypageStyle: CSS.Properties = {
+  margin: '0 auto',
+  float: 'right',
+  marginRight: '5px',
+};
 
 const Dashboard = () => {
-    return (
-        <>
-            <Layout className="layout">
-                <Header>
-                <div className="logo" />
-                  <Row style={mypageStyle}>
-                    <Col span={12}>
-                      <ChangePassword></ChangePassword>
-                    </Col>
-                    <Col span={12}>
-                      <Button danger >로그아웃</Button>
-                    </Col>
-                  </Row>
-                </Header>
-                <InstanceListContainer/>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-            </Layout>
-        </>
-    );
-}
+  const history = useHistory();
+  const handleOnClickLogout = useCallback(() => history.push('/login'), [
+    history,
+  ]);
+
+  return (
+    <>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Row style={mypageStyle}>
+            <Col span={12}>
+              <ChangePassword></ChangePassword>
+            </Col>
+            <Col span={12}>
+              <Button danger onClick={handleOnClickLogout}>
+                로그아웃
+              </Button>
+            </Col>
+          </Row>
+        </Header>
+        <InstanceListContainer />
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </>
+  );
+};
 
 export default Dashboard;
