0aed986c9fba81b13917f320ebab3f0fb6065aeb
wonpyoJang <jangwonpyo0@gmail.com>
2021-03-05 00:23:51 +0900

    [added] change password, publish and api binding

--- client/web/src/pages/ChangePassword/ChangePassword.tsx
+++ client/web/src/pages/ChangePassword/ChangePassword.tsx
@@ -3,10 +3,13 @@ import 'antd/dist/antd.css';
 import '../../index.css';
 import { Button, Modal, Form, Input } from 'antd';
 import CSS from 'csstype';
+import Password from 'antd/lib/input/Password';
+import { UserRepository } from '../../api/user/UserRepository';
 
-const mypageStyle: CSS.Properties = {
-  margin: '1rem',
-  float: 'right',
+const changePasswordButtonStyle: CSS.Properties = {
+  backgroundColor: 'rgba(255, 255, 255, 1)',
+  border: 'solid',
+  borderColor: 'rgba(78, 185, 242,1)',
 };
 
 const layout = {
@@ -24,28 +27,64 @@ const tailLayout = {
   },
 };
 
-const onFinish = (values: String) => {
-  console.log('Success:', values);
-};
-
 const onFinishFailed = () => {};
 
-const ChangePassword = () => {
+const ChangePassword = (props: any) => {
   const [visible, setVisible] = React.useState(false);
   const [confirmLoading, setConfirmLoading] = React.useState(false);
   const [modalText, setModalText] = React.useState('Content of the modal');
+  const [username, setUsername] = React.useState('');
+  const [currentPassword, setCurrentPassword] = React.useState('');
+  const [newPassword, setNewPassword] = React.useState('');
+  const [newPasswordConfirm, setNewPasswordConfirm] = React.useState('');
+
+  const changeUsernameHandler = (event: any) => {
+    console.log('event.target.value : ' + event.target.value);
+    setUsername(event.target.value);
+    console.log('username: ' + username);
+  };
+
+  const changeCurrentPasswordHandler = (event: any) => {
+    console.log('event.target.value : ' + event.target.value);
+    setCurrentPassword(event.target.value);
+    console.log('currentPassword: ' + currentPassword);
+  };
+
+  const changeNewPasswordHandler = (event: any) => {
+    console.log('event.target.value : ' + event.target.value);
+    setNewPassword(event.target.value);
+    console.log('newPassword: ' + newPassword);
+  };
+
+  const changeNewPasswordConfirmHandler = (event: any) => {
+    console.log('event.target.value : ' + event.target.value);
+    setNewPasswordConfirm(event.target.value);
+    console.log('newPasswordConfirm: ' + newPasswordConfirm);
+  };
 
   const showModal = () => {
     setVisible(true);
   };
 
   const handleOk = () => {
+    console.log('newPasswrd: ' + newPassword);
+    console.log('newPasswordConfirm: ' + newPasswordConfirm);
+    if (newPassword !== newPasswordConfirm) {
+      alert('새 패스워드와 새 패스워드 확인이 일치하지 않습니다.');
+      return;
+    }
+
+    let userRepository = new UserRepository();
+
+    let result = userRepository.postUser(currentPassword, newPassword);
+
     setModalText('The modal will be closed after two seconds');
     setConfirmLoading(true);
     setTimeout(() => {
       setVisible(false);
       setConfirmLoading(false);
     }, 2000);
+    return result;
   };
 
   const handleCancel = () => {
@@ -53,9 +92,11 @@ const ChangePassword = () => {
     setVisible(false);
   };
 
+  console.log('change password username : ' + props.username);
+
   return (
     <div>
-      <Button danger style={mypageStyle} onClick={showModal}>
+      <Button style={changePasswordButtonStyle} onClick={showModal}>
         비밀번호수정
       </Button>
       <Modal
@@ -71,8 +112,6 @@ const ChangePassword = () => {
           initialValues={{
             remember: true,
           }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
         >
           <Form.Item
             label="아이디"
@@ -84,7 +123,11 @@ const ChangePassword = () => {
               },
             ]}
           >
-            <Input />
+            <Input
+              disabled={true}
+              onChange={changeUsernameHandler}
+              defaultValue={props.username}
+            />
           </Form.Item>
           <Form.Item
             label="기존 비밀번호"
@@ -96,7 +139,7 @@ const ChangePassword = () => {
               },
             ]}
           >
-            <Input.Password />
+            <Input.Password onChange={changeCurrentPasswordHandler} />
           </Form.Item>
           <Form.Item
             label="변경할 비밀번호"
@@ -108,7 +151,7 @@ const ChangePassword = () => {
               },
             ]}
           >
-            <Input.Password />
+            <Input.Password onChange={changeNewPasswordHandler} />
           </Form.Item>
           <Form.Item
             label="비밀번호 재입력"
@@ -120,13 +163,7 @@ const ChangePassword = () => {
               },
             ]}
           >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
+            <Input.Password onChange={changeNewPasswordConfirmHandler} />
           </Form.Item>
         </Form>
       </Modal>
